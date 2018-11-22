424b45ba87b71020a36d5209caff4a8865d0ec9e
Manuel J <Major94@users.noreply.github.com>
2018-11-22 18:29:13 +0100

    cleanup

--- src/main/java/ch/major94/random_game/LaunchRandomGame.java
+++ src/main/java/ch/major94/random_game/LaunchRandomGame.java
@@ -1,27 +1,42 @@
 package ch.major94.random_game;
 
+import java.util.Random;
+
 import core.competition.CompetitionParameters;
 import tracks.ArcadeMachine;
 
 public class LaunchRandomGame {
+	
+	static boolean visuals = true;
+
+	static String sampleOLETSController = "tracks.singlePlayer.advanced.olets.Agent";
+	
+	static int run = 1;
+	static int gen = 24;
 
 	public static void main(String[] args) {
-
-		final boolean visuals = true;
-
-		String sampleOLETSController = "tracks.singlePlayer.advanced.olets.Agent";
-
-		CompetitionParameters.GAMETICKS = 0;
-
-		final int gen = 20;
-
+		
+		String run = "";
+		
+		try {
+			run = "run"+Integer.parseInt(args[0])+"/";
+			gen = Integer.parseInt(args[1]);
+			
+			System.out.println("parameters accepted");
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
 		//Use custom Game
-		final String game = "generated/"+gen+"game1.txt";
-		final String level1 = "generated/"+gen+"level1.txt";
-		final String recordActionsFile = "actions0.txt";
+		String game = run+"generated/"+gen+"game1.txt";
+		String level1 = run+"generated/"+gen+"level1.txt";
+		String recordActionsFile = null;//"actions0.txt";
+		
+		//CompetitionParameters.GAMETICKS = 100;
+		CompetitionParameters.ACTION_TIME = 40;
 
-		ArcadeMachine.runOneGame(game, level1, visuals, sampleOLETSController, recordActionsFile, 0, 0);
+		//ArcadeMachine.runOneGame(game, level1, visuals, sampleOLETSController, recordActionsFile, new Random().nextInt(), 0);
 		//ArcadeMachine.replayGame(game, level1, visuals, recordActionsFile);
-		//ArcadeMachine.playOneGame(game, level1, null, 0);
+		ArcadeMachine.playOneGame(game, level1, null, new Random().nextInt());
 	}
 }
